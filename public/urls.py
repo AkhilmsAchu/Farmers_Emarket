0cfd08e03e8e936034dd92f4a3a4804346a671db
akhilmsachu <akhilsree001@gmail.com>
2020-02-19 21:11:27 +0530

    added shop, updated links on index & shop

--- public/urls.py
+++ public/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name='home'),
+    path('index', views.index,name='index'),
+    path('shop', views.shop,name='shop'),
 ]
--- public/views.py
+++ public/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 
 def index(request):
 	return render(request,"public/index.html")
+def shop(request):
+	return render(request,"public/shop.html")
