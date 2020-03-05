ad6f89537171f8466f8f8d89f3312ea2c43bdae0
Akhil MS <Akhilsree001@gmail.com>
2020-03-05 20:56:26 +0530

    added initial support for user registration (no validation)

--- farmers/views.py
+++ farmers/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
 # Create your views here.
 
 def login(request):
 	return render(request,"farmers/login.html")
 def signup(request):
-	return render(request,"farmers/signup.html")
+	if request.method == 'POST':
+		first_name=request.POST['first_name']
+		last_name=request.POST['last_name']
+		email=request.POST['email']
+		password=request.POST['password']
+		username=request.POST['email']
+
+		user =User.objects.create_user(username=username,first_name=first_name,last_name=last_name,email=email,password=password)
+		user.save();
+		print('created')
+		return redirect('/')
+	else:
+		return render(request,"farmers/signup.html")
+		print('failed')
