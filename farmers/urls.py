e53a5882cfc876a155df9e2ce0c0b13da399d456
Akhil MS <Akhilsree001@gmail.com>
2020-03-05 21:33:56 +0530

    added initial support for user login and logout

--- farmers/urls.py
+++ farmers/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login,name='farmers_login'),
-    path('login', views.login,name='farmers_login'),
-    path('signup', views.signup,name='farmers_signup'),
 ]
--- public/views.py
+++ public/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
 # Create your views here.
 
 def index(request):
@@ -22,3 +23,34 @@ def ourfarmers(request):
 	return render(request,"public/ourfarmers.html")
 def farmerdetails(request):
 	return render(request,"public/ourfarmers_details.html")
+
+def logout(request):
+	auth.logout(request)
+	return redirect('/')
+
+def login(request):
+	if request.method == 'POST':
+		password=request.POST['password']
+		username=request.POST['username']
+		user=auth.authenticate(username=username,password=password)
+		if user is not None:
+			auth.login(request,user)
+			return redirect('/')
+		else :
+			return redirect('/login')
+	else:
+		return render(request,"farmers/login.html")
+def signup(request):
+	if request.method == 'POST':
+		first_name=request.POST['first_name']
+		last_name=request.POST['last_name']
+		email=request.POST['email']
+		password=request.POST['password']
+		username=request.POST['email']
+
+		user =User.objects.create_user(username=username,first_name=first_name,last_name=last_name,email=email,password=password)
+		user.save();
+		print('created')
+		return redirect('/login')
+	else:
+		return render(request,"farmers/signup.html")
