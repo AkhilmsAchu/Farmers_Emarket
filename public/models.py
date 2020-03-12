a80b023074637dd7c2068c03693cfb773f64f19e
Akhil MS <Akhilsree001@gmail.com>
2020-03-12 12:36:56 +0530

    made migration for cart in public

--- public/models.py
+++ public/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from farmers.models import products
 # Create your models here.
+
+
+class cart(models.Model):
+	userid = models.ForeignKey(User,default=None,on_delete=models.DO_NOTHING)
+	productid = models.ForeignKey(products,default=None,on_delete=models.DO_NOTHING)
+	quantity = models.IntegerField()
