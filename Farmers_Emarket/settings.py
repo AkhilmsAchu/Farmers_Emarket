74d627783ce09f5a66a9cebe83a3f08fb5c91d3f
Akhil MS <akhilsree001@gmail.com>
2020-07-31 15:59:12 +0530

    moved to sqlite3 db

--- Farmers_Emarket/settings.py
+++ Farmers_Emarket/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'Farmers_Emarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'farmersemarket',
-        'USER': 'postgres',
-        'PASSWORD' : 'admin',
-        'HOST':  'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
